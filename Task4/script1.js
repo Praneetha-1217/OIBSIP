068363a310147c1a70cbbbc27a5a23e375bb99dc
Praneetha-1217 <gadelapraneetha21@gmail.com>
2024-06-23 22:36:34 +0530

    Update script1.js

--- Task4/script1.js
+++ Task4/script1.js
@@ -3,8 +3,7 @@ function register() {
   var username = document.getElementById('registerUsername').value;
   var password = document.getElementById('registerPassword').value;
 
-  // Check if username already exists
-  if (users.some(user.username === username)) {
+  if (users.some(user=>user.username === username)) {
     window.alert('Username already exists. Please choose another one.');
     return;
   }
@@ -12,10 +11,8 @@ function register() {
 function login() {
   var username = document.getElementById('loginUsername').value;
   var password = document.getElementById('loginPassword').value;
-
-  // Check if credentials match
-  var user = users.find(user.username === username && user.password === password);
-
+  
+  var user = users.find(user=>user.username === username && user.password === password);
   if (user) {
     window.alert('Login successful!');
     showSecuredPage();
@@ -23,15 +20,13 @@ function login() {
     window.alert('Invalid username or password. Please try again.');
   }
 }
-
 function showLoginForm() {
   document.getElementById('loginForm').style.display = 'block';
   document.getElementById('registerForm').style.display = 'none';
   document.getElementById('securedPage').style.display = 'none';
 }
-
 function showSecuredPage() {
-  document.getElementById('loginForm').style.display = 'none';
+  document.getElementById('loginForm').style.display = 'loginsuccessfull';
   document.getElementById('registerForm').style.display = 'none';
   document.getElementById('securedPage').style.display = 'block';
 }
